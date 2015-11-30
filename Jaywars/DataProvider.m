4b09a3c9b48100fd244b177e699f7b301d53944f
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-30 14:01:22 +0100

    simply dictionary caching

--- Jaywars/DataProvider.m
+++ Jaywars/DataProvider.m
@@ -9,9 +9,13 @@
 #import "DataProvider.h"
 #import "SWAPI.h"
 
+
+static NSString * const allMovies = @"allMoviesKey";
+
 @interface DataProvider()
 
 @property (nonatomic, strong) NSOperationQueue *dataQueue;
+@property (nonatomic, strong) NSMutableDictionary *dummyCache;
 
 @end
 
@@ -27,6 +31,7 @@
         dataProvider.dataQueue = [[NSOperationQueue alloc] init];
         dataProvider.dataQueue.maxConcurrentOperationCount = 5;
         dataProvider.dataQueue.name = @"Jaywars.DataQueue";
+        dataProvider.dummyCache = [[NSMutableDictionary alloc] init];
     });
     
     return dataProvider;
@@ -34,34 +39,47 @@
 
 -(void)fetchMoviesWithCompletionBlock:(void (^)(NSArray<SWFilm*> *movies))completion {
     [self.dataQueue addOperationWithBlock:^{
-        [SWAPI getFilmsWithCompletion:^(SWResultSet *result, NSError *error) {
-            if (error) {
-                NSLog(@"An error occured, %@", [error debugDescription]);
-            } else {
-                if (completion) {
-                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                        NSArray *sorted = [result.items sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"episodeID" ascending:YES]]];
-                        completion(sorted);
-                    }];
-
+        if (completion && [self.dummyCache valueForKey:allMovies]) {
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                completion([self.dummyCache valueForKey:allMovies]);
+            }];
+        } else {
+            [SWAPI getFilmsWithCompletion:^(SWResultSet *result, NSError *error) {
+                if (error) {
+                    NSLog(@"An error occured, %@", [error debugDescription]);
+                } else {
+                    if (completion) {
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            NSArray *sorted = [result.items sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"episodeID" ascending:YES]]];
+                            [self.dummyCache setValue:sorted forKey:allMovies];
+                            completion(sorted);
+                        }];
+                        
+                    }
                 }
-            }
-        }];
+            }];
+        }
     }];
 }
 
 - (void)fetchCharactersForFilm:(SWFilm *)film withCompletionBlock:(void (^)(NSArray<SWPerson*> *characters))completion {
     [self.dataQueue addOperationWithBlock:^{
-        [film getCharactersWithCompletion:^(SWResultSet *result, NSError *error) {
-            if (completion) {
-                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                    NSArray *sorted = [result.items sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]]];
-                    completion(sorted);
-                }];
-                
-            }
-        }];
-        
+        if (completion && [self.dummyCache valueForKey:film.URLString]) {
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                completion([self.dummyCache valueForKey:film.URLString]);
+            }];
+        } else {
+            [film getCharactersWithCompletion:^(SWResultSet *result, NSError *error) {
+                if (completion) {
+                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                        NSArray *sorted = [result.items sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]]];
+                        [self.dummyCache setValue:sorted forKey:film.URLString];
+                        completion(sorted);
+                    }];
+                    
+                }
+            }];
+        }
     }];
 }
 
