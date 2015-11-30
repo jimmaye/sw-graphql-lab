4f70ffd569a9b8edbec01537a3b0d601ce69dddc
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-30 08:56:32 +0100

    Initial Commit

---
+++ Jaywars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jaywars
+//
+//  Created by Jimmie Jensen on 30/11/15.
+//  Copyright © 2015 Jayway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jaywars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jaywars
+//
+//  Created by Jimmie Jensen on 30/11/15.
+//  Copyright © 2015 Jayway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
