0459756eafeb8237577336b061a9a87c6ea84782
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-30 08:59:45 +0100

    added dataprovider for further implementation

---
+++ Jaywars/DataProvider.m
@@ -0,0 +1,13 @@
+//
+//  DataProvider.m
+//  Jaywars
+//
+//  Created by Jimmie Jensen on 30/11/15.
+//  Copyright © 2015 Jayway. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+@implementation DataProvider
+
+@end
