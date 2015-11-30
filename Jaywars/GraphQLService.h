6e6a3f339bc829c7a7bbf0d8cc03c6273568da50
Jimmie Jensen <jimmie.jensen@jayway.com>
2015-11-30 15:05:44 +0100

    adde graphql lib

---
+++ Jaywars/GraphQLService.h
@@ -0,0 +1,13 @@
+//
+//  GraphQLService.h
+//  Jaywars
+//
+//  Created by Jimmie Jensen on 30/11/15.
+//  Copyright © 2015 Jayway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GraphQLService : NSObject
+
+@end
---
+++ Jaywars/GraphQLService.m
@@ -0,0 +1,13 @@
+//
+//  GraphQLService.m
+//  Jaywars
+//
+//  Created by Jimmie Jensen on 30/11/15.
+//  Copyright © 2015 Jayway. All rights reserved.
+//
+
+#import "GraphQLService.h"
+
+@implementation GraphQLService
+
+@end
